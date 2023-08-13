785ad9c7e479359a1292edad7a009420e88f130f
Himanshu Sahu <109578416+himanshu20163@users.noreply.github.com>
2023-08-13 15:38:21 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import logo from './logo.svg';
 import './App.css';
-import All_cards from './Components/React-Props-001/All_cards';
-import Blogcomponent from './Components/React-Props-002/Blogcomponent';
-import Allblog from './Components/React-Props-002/Allblog';
 import Toggle from './Components/React-state-001/Toggle';
 // import Profile from './Components/React-Component-001/Profile';
 
